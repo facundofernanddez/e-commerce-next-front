7f1a5402900e41be7f4f6c5c3c14322dea1c0e8b
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2023-08-03 20:36:11 -0300

    first things for nav

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,1 +1,25 @@
-export default function Header() {}
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <>
+      <div className="bg-[#090908] dark:bg-white text-white dark:text-gray-900">
+        <header className="max-w-6xl mx-auto pl-4">
+          <Link
+            className=""
+            href={"/"}
+          >
+            e-commerce
+          </Link>
+          <nav>
+            <Link href={"/"}>Home</Link>
+            <Link href={"/products"}>Al products</Link>
+            <Link href={"/categories"}>Categories</Link>
+            <Link href={"/account"}>Account</Link>
+            <Link href={"/cart"}>Cart (0)</Link>
+          </nav>
+        </header>
+      </div>
+    </>
+  );
+}
